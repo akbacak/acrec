e7b6aa96423555f6a66653b3733abeac916e6be0
akbacak <33653031+akbacak@users.noreply.github.com>
2020-01-09 11:26:19 +0300

    Add files via upload

---
+++ extract_frames_v3.py
@@ -0,0 +1,36 @@
+import os
+import math
+import cv2
+import re
+listing = os.listdir("/home/ubuntu/keras/enver/acrec/videos/")
+count = 1
+
+for file in listing:
+    train_val_dir  = os.listdir("/home/ubuntu/keras/enver/acrec/videos/" + file + "/" )   
+    for file_2 in train_val_dir:
+       class_dir = os.listdir("/home/ubuntu/keras/enver/acrec/videos/" + file + "/" + file_2 + "/") 
+       for videos in class_dir:
+           cap = cv2.VideoCapture("/home/ubuntu/keras/enver/acrec/videos/" + file + "/" + file_2 + "/" + videos)
+           os.makedirs("/home/ubuntu/keras/enver/acrec/Frames/" + file + "/" + file_2 + "/" + videos)
+           fps = cap.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
+           frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+           duration = frame_count/fps
+           def getFrame(sec):
+               cap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+               hasFrames,image = cap.read()
+               if hasFrames:
+                   e=cv2.imwrite("/home/ubuntu/keras/enver/acrec/Frames/"+ file +"/" + file_2 + "/" + videos +"/" + videos + "_" +str(count)+".jpg", image)
+               return hasFrames
+           sec = 0
+           N = 10 # How many frames will be extracted
+           interwall = duration/N   #//it will capture image in each 'interwall' second
+           count = 1
+           success = getFrame(sec)
+           while success:
+               count = count + 1
+               sec = sec + interwall
+               success = getFrame(sec)
+
+
+os.system('resize.sh')
+
