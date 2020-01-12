37296d58e41bef3ad66e2236c6c7100c7cea5335
akbacak <33653031+akbacak@users.noreply.github.com>
2020-01-12 15:06:23 +0300

    Add files via upload

--- extract_frames_v3.py
+++ extract_frames_v3.py
@@ -1,17 +1,18 @@
+# Extract N frames from videos's subfolder and save then Frames's respective folder, then resize all frames
 import os
 import math
 import cv2
 import re
-listing = os.listdir("/home/ubuntu/keras/enver/acrec/videos/")
+listing = os.listdir("./videos/")
 count = 1
 
 for file in listing:
-    train_val_dir  = os.listdir("/home/ubuntu/keras/enver/acrec/videos/" + file + "/" )   
+    train_val_dir  = os.listdir("./videos/" + file + "/" )   
     for file_2 in train_val_dir:
-       class_dir = os.listdir("/home/ubuntu/keras/enver/acrec/videos/" + file + "/" + file_2 + "/") 
+       class_dir = os.listdir("./videos/" + file + "/" + file_2 + "/" ) 
        for videos in class_dir:
-           cap = cv2.VideoCapture("/home/ubuntu/keras/enver/acrec/videos/" + file + "/" + file_2 + "/" + videos)
-           os.makedirs("/home/ubuntu/keras/enver/acrec/Frames/" + file + "/" + file_2 + "/" + videos)
+           cap = cv2.VideoCapture("./videos/" + file + "/" + file_2 + "/" + videos)
+           os.makedirs("./activity_data/" + file + "/" + file_2 + "/" + videos)
            fps = cap.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
            duration = frame_count/fps
@@ -19,10 +20,10 @@ for file in listing:
                cap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
                hasFrames,image = cap.read()
                if hasFrames:
-                   e=cv2.imwrite("/home/ubuntu/keras/enver/acrec/Frames/"+ file +"/" + file_2 + "/" + videos +"/" + videos + "_" +str(count)+".jpg", image)
+                   e=cv2.imwrite("./activity_data/"+ file +"/" + file_2 + "/" + videos +"/" + videos + "_" +str(count)+".jpg", image)
                return hasFrames
            sec = 0
-           N = 10 # How many frames will be extracted
+           N = 16 # How many frames will be extracted
            interwall = duration/N   #//it will capture image in each 'interwall' second
            count = 1
            success = getFrame(sec)
--- resize.sh
+++ resize.sh
@@ -1,4 +1,4 @@
-for f in "/home/ubuntu/keras/enver/acrec/Frames/*/*/*/*.jpg"
+for f in "/home/ubuntu/keras/enver/acrec/custom_data_generator/activity_recognition/activity_data/*/*/*/*.jpg"
 do
      mogrify $f -resize 224x224! $f
 done
